8b070f99eade7b78566bc13437a1c21faddf7d82
tuvshuu <chi.tuvshinbayar@gmail.com>
2021-01-03 11:34:18 +0800

    Shoog shiddeg bolgoson

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 // Шооны аль талаар буусныг хадгалах хувьсагч. Гэхдээ 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
 
-// <div class="player-score" id="score-0">43</div>
-// window.document.querySelector('#score-0').textContent = dice; //DOC буюу window.document дотор querySelector хайлт хийдэг.
-// document.querySelector('#score-1').innerHTML = '<em>hi</em>';
+
 // Программ эхлэхэд бэлтгэе
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-document.querySelector('.dice').style.display = 'none';
-console.log('Шоо : ' + dice);
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+var diceDom = document.querySelector('.dice');
+// document.getElementById('.dice').style.display = 'none';
+// Anonymous функц
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + diceNumber + '.png';
+    // alert('Шоо шидлээ: ' + diceNumber);
+});
